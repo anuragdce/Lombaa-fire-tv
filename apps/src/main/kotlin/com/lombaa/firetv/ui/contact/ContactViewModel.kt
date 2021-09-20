7c4e50d9251909f7a3619935b218d442f2415532
anurag <anuragdce@gmail.com>
2021-09-20 14:51:13 +0530

    added contactus ui.

--- apps/src/main/kotlin/com/lombaa/firetv/ui/contact/ContactViewModel.kt
+++ apps/src/main/kotlin/com/lombaa/firetv/ui/contact/ContactViewModel.kt
@@ -2,6 +2,8 @@ package com.lombaa.firetv.ui.contact
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -11,6 +13,10 @@ import javax.inject.Inject
 internal class ContactViewModel @Inject constructor(
   application: Application,
 ) : AndroidViewModel(application) {
+
+  val email: LiveData<String> = MutableLiveData("abc@gmail.com")
+  val phone: LiveData<String> = MutableLiveData("+91 9876543210")
+  val address: LiveData<String> = MutableLiveData("Colaborar Labs, Noida One Building, Sector 62, Noida.")
 
   init {
     viewModelScope.launch {
