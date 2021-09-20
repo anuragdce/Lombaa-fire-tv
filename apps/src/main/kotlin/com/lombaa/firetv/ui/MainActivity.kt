dc45029d0a53f886c708e714b964b18764fdc474
anurag <anuragdce@gmail.com>
2021-09-20 16:02:30 +0530

    added default fragment as home

--- apps/src/main/kotlin/com/lombaa/firetv/ui/MainActivity.kt
+++ apps/src/main/kotlin/com/lombaa/firetv/ui/MainActivity.kt
@@ -11,52 +11,50 @@ import androidx.appcompat.app.AppCompatActivity
 import com.lombaa.firetv.R
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
 internal class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    viewGoneAnimator(findViewById(R.id.nav_menu_fragment))
-  }
-
-
-  override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
-    if (event != null) {
-      when (event.keyCode) {
-        KEYCODE_DPAD_LEFT -> {
-          viewGoneAnimator(findViewById(R.id.nav_menu_fragment))
-          return true
-        }
-        KEYCODE_DPAD_RIGHT -> {
-          viewVisibleAnimator(findViewById(R.id.nav_menu_fragment))
-          return true
-        }
-      }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewGoneAnimator(findViewById(R.id.nav_menu_fragment))
     }
 
-    return super.dispatchKeyEvent(event)
-  }
-
-  private fun viewGoneAnimator(view: View) {
-    view.animate()
-      .alpha(0f)
-      .setDuration(1000)
-      .setListener(object : AnimatorListenerAdapter() {
-        override fun onAnimationEnd(animation: Animator) {
-          view.visibility = View.GONE
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        if (event != null) {
+            when (event.keyCode) {
+                KEYCODE_DPAD_LEFT -> {
+                    viewGoneAnimator(findViewById(R.id.nav_menu_fragment))
+                    return true
+                }
+                KEYCODE_DPAD_RIGHT -> {
+                    viewVisibleAnimator(findViewById(R.id.nav_menu_fragment))
+                    return true
+                }
+            }
         }
-      })
-  }
 
-  private fun viewVisibleAnimator(view: View) {
-    view.animate()
-      .alpha(1f)
-      .setDuration(1000)
-      .setListener(object : AnimatorListenerAdapter() {
-        override fun onAnimationEnd(animation: Animator) {
-          view.visibility = View.VISIBLE
-        }
-      })
-  }
+        return super.dispatchKeyEvent(event)
+    }
+
+    private fun viewGoneAnimator(view: View) {
+        view.animate()
+            .alpha(0f)
+            .setDuration(1000)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    view.visibility = View.GONE
+                }
+            })
+    }
+
+    private fun viewVisibleAnimator(view: View) {
+        view.animate()
+            .alpha(1f)
+            .setDuration(1000)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    view.visibility = View.VISIBLE
+                }
+            })
+    }
 }
