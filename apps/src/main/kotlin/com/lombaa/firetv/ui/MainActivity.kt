272c63c799ffe43789f76cbafb355bf35d4c004a
anurag <anuragdce@gmail.com>
2021-09-20 16:17:58 +0530

    added drawer layout

--- apps/src/main/kotlin/com/lombaa/firetv/ui/MainActivity.kt
+++ apps/src/main/kotlin/com/lombaa/firetv/ui/MainActivity.kt
@@ -1,33 +1,29 @@
 package com.lombaa.firetv.ui
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_DPAD_LEFT
-import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
-import android.view.View
+import android.view.KeyEvent.KEYCODE_MENU
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.lombaa.firetv.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 internal class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewGoneAnimator(findViewById(R.id.nav_menu_fragment))
+        drawerLayout = findViewById(R.id.drawer_layout)
     }
 
     override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
         if (event != null) {
             when (event.keyCode) {
-                KEYCODE_DPAD_LEFT -> {
-                    viewGoneAnimator(findViewById(R.id.nav_menu_fragment))
-                    return true
-                }
-                KEYCODE_DPAD_RIGHT -> {
-                    viewVisibleAnimator(findViewById(R.id.nav_menu_fragment))
+                KEYCODE_MENU, KEYCODE_DPAD_LEFT -> {
+                    drawerLayout.open()
                     return true
                 }
             }
@@ -36,25 +32,11 @@ internal class MainActivity : AppCompatActivity() {
         return super.dispatchKeyEvent(event)
     }
 
-    private fun viewGoneAnimator(view: View) {
-        view.animate()
-            .alpha(0f)
-            .setDuration(1000)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    view.visibility = View.GONE
-                }
-            })
-    }
-
-    private fun viewVisibleAnimator(view: View) {
-        view.animate()
-            .alpha(1f)
-            .setDuration(1000)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    view.visibility = View.VISIBLE
-                }
-            })
+    override fun onBackPressed() {
+        if (drawerLayout.isOpen) {
+            drawerLayout.close()
+            return
+        }
+        super.onBackPressed()
     }
 }
